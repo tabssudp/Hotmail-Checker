ff055615d235f3294ff82131c696af55e0c11577
T A B $ $ <137601276+tabssudp@users.noreply.github.com>
2024-10-20 19:15:18 +0200

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+
+pip install -r requirements.txt
+
+echo [ESC][32m¡Las dependencias se han instalado correctamente! Ahora puedes usar la herramienta.[ESC][0m
+
+echo ^[32m¡Las dependencias se han instalado correctamente! Ahora puedes usar la herramienta.^[0m
+
+:: Inicia la herramienta
+python hotmail.py
